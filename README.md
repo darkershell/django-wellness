1aa45cda4ca8967c23cba311891766645350a66f
Richard Nixon <rnixonaf@gmail.com>
2023-06-13 22:32:23 +0100

    user model

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # django-wellness
+<<<<<<< HEAD
 
 [![.github/workflows/django.yml](https://github.com/darkershell/django-wellness/actions/workflows/django.yml/badge.svg)](https://github.com/darkershell/django-wellness/actions/workflows/django.yml)
 [![Updates](https://pyup.io/repos/github/darkershell/django-wellness/shield.svg)](https://pyup.io/repos/github/darkershell/django-wellness/)
@@ -6,3 +7,6 @@
 
 
 track my personal wellness habits
+=======
+Track my personal wellness habits
+>>>>>>> 1f246c7 (add user model + decouple)
--- wellness/settings.py
+++ wellness/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
+from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +28,8 @@ SECRET_KEY = 'django-insecure-pu7kozxu0_1hri#36glqef5^qja80!*x6hqob7n9h3p6zior5l
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+AUTH_USER_MODEL = 'core.User'
 
 
 # Application definition
