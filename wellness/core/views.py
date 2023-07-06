f33c7177904160714023d49803c374dd55a8463d
Richard Nixon <rnixonaf@gmail.com>
2023-07-06 21:11:04 +0100

    Add js, css and home template

--- wellness/core/views.py
+++ wellness/core/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<html><body>Ola Django</body></html>', content_type='text/html')
+    return render(request, 'core/home.html')
