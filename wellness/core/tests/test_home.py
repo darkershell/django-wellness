f76ea6b981876182c075fad8dbda4cf4bc089c39
Richard Nixon <rnixonaf@gmail.com>
2023-07-07 21:26:08 +0100

    test django assertions

---
+++ wellness/core/tests/test_home.py
@@ -0,0 +1,12 @@
+from django.test import Client
+from wellness.django_assertions import assert_contains
+
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Django Wellness</title>')
